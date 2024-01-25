777288c971df4de5539d7ec03c9a718a08fff33c
Tim Bulokhov <timofeybrant@mail.ru>
2024-01-25 21:27:02 +0200

    Создал файлы обратно

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,8 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Timofey Bulokhov on 25.01.2024.
+//
+
+import Foundation
---
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ImagesListViewController.swift
+//  ImageFeed
+//
+//  Created by Timofey Bulokhov on 25.01.2024.
+//
+
+import Foundation
