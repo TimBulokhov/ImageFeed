56844c1e8d89d97b146d3a8d616c9d5b1ebbdee1
Tim Bulokhov <timofeybrant@mail.ru>
2024-01-25 21:32:59 +0200

    Переписал файлы заново

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -5,4 +5,12 @@
 //  Created by Timofey Bulokhov on 25.01.2024.
 //
 
-import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var likeButton: UIButton!
+}
+
