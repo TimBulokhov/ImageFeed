577001df30b11d4b69dee091904daa6f322507aa
Tim Bulokhov <timofeybrant@mail.ru>
2024-02-04 14:40:57 +0200

    Added new assets (tabbar icons), UITabBarController is set up.

--- ImageFeed/UserProfile/ProfileViewController.swift
+++ ImageFeed/UserProfile/ProfileViewController.swift
@@ -1,5 +1,5 @@
 //
-//  UserProfile.swift
+//  ProfileViewController.swift
 //  ImageFeed
 //
 //  Created by Timofey Bulokhov on 04.02.2024.
@@ -8,5 +8,16 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
+
+    @IBOutlet private var avatarImageView: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
     
-}
+    @IBOutlet var logOutButton: UIButton!
+    
+    
+    @IBOutlet var didTapLogOutButton: UIButton!
+    
+    }
+
