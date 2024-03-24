8f508d56f0263daf760ce764ad75654e1306e3a4
Tim Bulokhov <timofeybrant@mail.ru>
2024-03-24 17:15:18 +0200

    Zoom is working, but share button is dropping app, need to fix it

--- ImageFeed/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 import Kingfisher
 
 final class SingleImageViewController: UIViewController {
+    private lazy var zoomImage: imageZoomer = imageZoomer()
+    private let kfManager = KingfisherManager.shared
+    private var image: UIImage?
     
     var imageURL: URL! {
         didSet {
@@ -17,74 +20,82 @@ final class SingleImageViewController: UIViewController {
         }
     }
     
-    @IBOutlet private weak var scrollView: UIScrollView!
-    @IBOutlet private var imageView: UIImageView!
-    
-    @IBAction private func didTapBackButton(_ sender: UIButton) {
-        dismiss(animated: true, completion: nil)
-    }
-    
-    @IBAction private func didTapShareButton(_ sender: UIButton) {
-        let share = UIActivityViewController(activityItems: [imageView.image as Any], applicationActivities: nil)
-        present(share, animated: true, completion: nil)
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setImage()
-        scrollView.delegate = self
-        scrollView.minimumZoomScale = 1.0
-        scrollView.maximumZoomScale = 3.0
-    }
-}
-
-extension SingleImageViewController: UIScrollViewDelegate {
-    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        imageView
-    }
-}
-
-extension SingleImageViewController {
-    
-    private func setImage() {
-        UIBlockingProgressHUD.show()
-        imageView.kf.setImage(with: imageURL) { [weak self] result in
-            UIBlockingProgressHUD.dismiss()
+    private lazy var shareImageButton: UIButton = {
+        let shareImageButton = UIButton.systemButton(
+            with: UIImage(named: "share_button") ?? UIImage(),
+            target: self,
+            action: #selector(didTapShareButton)
+            )
+            shareImageButton.layer.cornerRadius = 25.0
+            shareImageButton.backgroundColor = .ypBlack
+            return shareImageButton
+            }()
             
-            guard let self else { return }
-            switch result {
-            case .success(let imageResult):
-                self.rescaleAndCenterImageInScrollView(image: imageResult.image)
-            case .failure:
-                self.showAlert()
+            private lazy var backToListButton: UIButton = {
+                let backToListButton = UIButton.systemButton(
+                    with: UIImage(named: "back_button") ?? UIImage(),
+                    target: self,
+                    action: #selector(didTapBackButton)
+                )
+                backToListButton.tintColor = .ypWhite
+                return  backToListButton
+            }()
+            
+            @IBAction func didTapShareButton(_ sender: UIButton) {
+                guard let image else { return }
+                let activityViewController = UIActivityViewController(activityItems: [image, self], applicationActivities: nil)
+                activityViewController.popoverPresentationController?.sourceView = self.view
+                self.present(activityViewController, animated: true, completion: nil)
             }
-        }
-    }
-    
-    private func rescaleAndCenterImageInScrollView(image: UIImage) {
-        let minZoomScale = scrollView.minimumZoomScale
-        let maxZoomScale = scrollView.maximumZoomScale
-        view.layoutIfNeeded()
-        let visibleRectSize = scrollView.bounds.size
-        let imageSize = image.size
-        let hScale = visibleRectSize.width / imageSize.width
-        let vScale = visibleRectSize.height / imageSize.height
-        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
-        scrollView.setZoomScale(scale, animated: false)
-        scrollView.layoutIfNeeded()
-        let newContentSize = scrollView.contentSize
-        let x = (newContentSize.width - visibleRectSize.width) / 2
-        let y = (newContentSize.height - visibleRectSize.height) / 2
-        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
-    }
-    
-    func showAlert() {
-        let alert = UIAlertController(title: "Download error", message: "Something was wrong, please try again :(", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Back", style: .default))
-        alert.addAction(UIAlertAction(title: "Retry", style: .default, handler: { (action: UIAlertAction) in
-            self.setImage()
-        }))
-        self.present(alert, animated: true)
-    }
-}
-
+            @IBAction private func didTapBackButton(_ sender: UIButton) {
+                dismiss(animated: true, completion: nil)
+            }
+            
+            override func viewDidLoad() {
+                super.viewDidLoad()
+                addSubViews()
+                addImageConstraints()
+                setImage()
+            }
+            
+            private func addSubViews() {
+                zoomImage.translatesAutoresizingMaskIntoConstraints = false
+                view.insertSubview(zoomImage, at: 0)
+            }
+            
+            private func addImageConstraints() {
+                NSLayoutConstraint.activate([
+                    zoomImage.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                    zoomImage.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                    zoomImage.topAnchor.constraint(equalTo: view.topAnchor),
+                    zoomImage.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+                ])
+            }
+            }
+            
+            extension SingleImageViewController {
+                
+                private func setImage() {
+                    UIBlockingProgressHUD.show()
+                    
+                    kfManager.retrieveImage(with: imageURL) { [weak self] result in
+                        switch result {
+                        case .success(let value):
+                            self?.zoomImage.downloadStartImage(value.image)
+                        case .failure:
+                            break
+                        }
+                        UIBlockingProgressHUD.dismiss()
+                    }
+                }
+                
+                func showAlert() {
+                    let alert = UIAlertController(title: "Download error", message: "Something was wrong, please try again :(", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Back", style: .default))
+                    alert.addAction(UIAlertAction(title: "Retry", style: .default, handler: { (action: UIAlertAction) in
+                        self.setImage()
+                    }))
+                    self.present(alert, animated: true)
+                }
+            }
+            
